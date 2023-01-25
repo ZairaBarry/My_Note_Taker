b21cf511899145c0399965134779fcca0a29820b
Zi <104781216+ZairaBarry@users.noreply.github.com>
2023-01-25 19:51:50 -0400

    created HTML routes

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const path = require ('path');
 const fs = require('fs');
 
 
-const { v4: uuidv4 } = require ('uuid')
+const htmlRoutes = require ('./routes/htmlRoutes');
+const apiRoutes = require ('./routes/apiRoutes');
+
+
 
 
 const app = express()
@@ -12,3 +15,12 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({extended: true}))
 app.use(express.json());
 app.use(express.static('public'));
+
+app.use ('/api' , apiRoutes);
+app.use('/', htmlRoutes);
+
+
+
+
+app.listen(PORT, () =>
+ console.log(`Listening on PORT ${PORT}`));
