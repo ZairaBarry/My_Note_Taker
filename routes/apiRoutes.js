430ca908783291c9243164d9db33140fb9cc09ad
Zi <104781216+ZairaBarry@users.noreply.github.com>
2023-01-26 18:24:03 -0400

    updated version

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -21,15 +21,19 @@ router.post('/notes', (req, res) => {
 });
 
 router.delete('/notes/:id', (req, res) => {
+    const savedNote = db;
+    // fs.readFile(path.join(__dirname, "../db/db.json"), 'utf8', (err, data) => {
+    //     if(err) throw err;
+    //     return res.json(JSON.parse(savedNote))
+    // })
+
     const idParams = req.params.id;
-  
-    const deletedId = db.filter( notes => notes.id != idParams);
+    const deletedId = savedNote.filter( notes => notes.id !== idParams);
 
     fs.writeFileSync(path.join(__dirname, "../db/db.json"), JSON.stringify(deletedId))
     res.json(deletedId);
+    
    
 })
-
-
 
 module.exports = router;
