6a32591be33abd765863db7c677395b5e6241c0d
Zi <104781216+ZairaBarry@users.noreply.github.com>
2023-01-30 17:34:49 -0400

    deploing into heroku

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express = require ('express');
-const path = require ('path');
+const express = require('express');
+const path = require('path');
 const fs = require('fs');
 const PORT = process.env.PORT || 3001;
 
-const htmlRoutes = require ('./routes/htmlRoutes');
-const apiRoutes = require ('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes');
 
 
 
@@ -12,15 +12,15 @@ const apiRoutes = require ('./routes/apiRoutes');
 const app = express()
 
 
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 app.use(express.static('public'));
 
-app.use ('/api' , apiRoutes);
+app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
 
 
 
 app.listen(PORT, () =>
- console.log(`Listening on PORT ${PORT}`));
+    console.log(`Listening on PORT ${PORT}`));
